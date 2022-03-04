Bug: Fix scaling of model extraction, and stopped made overlay face explicit. - There was a bug in which padding was being applied with a constant value, but the image being padded changed size. This led to a changing percentage of image being padded, which led to overlays having inconsistent size. Updated padding to scale with image dimensions. - When multiple faces were in screen, focus would sometimes flicker between the two. I added a sort on face size, so that the largest faces always gets the model / overlay.

LiveFaceSearch.py
@@ -11,15 +11,18 @@ class ImageProcess:
         self.avg_duration = avg
         self.avg_pts = np.zeros((68, 2))
 
-    def update_frame(self, image, model):
+    def update_and_display_frame(self, image, model, face_cascade):
         # load in a haar cascade classifier for detecting frontal faces
-        face_cascade = cv2.CascadeClassifier('detector_architectures/haarcascade_frontalface_default.xml')
         faces = face_cascade.detectMultiScale(image, 1.2, 10)
 
 
         if len(faces) > 0: # Let's play with the first face detection a bit more
+            faces = sorted(faces, key=lambda x: x[2], reverse=True)
+            # for face in faces:
             roi_meta = faces[0]
             roi = display_tools.extract_roi(image, roi_meta)
+            if roi is None:
+                return
             key_points = model.find_keypoints(roi) # Keypoints in model-space
             if(self.avg_pts[0][0] == 0):
                 self.avg_pts = key_points
@@ -27,22 +30,29 @@ class ImageProcess:
                 self.avg_pts = (self.avg_pts * (self.avg_duration - 1) + key_points) / self.avg_duration
             roi_pts = np.array([converters.roi_from_model(pt, roi.shape) for pt in self.avg_pts])
 
-            display_tools.overlay_points(cv2.resize(roi, (224, 224)),
-                                         self.avg_pts,
-                                         name="ModelSpace")
-            display_tools.overlay_points(roi, roi_pts, name="RoiSpace")
+            model_space_image = display_tools.overlay_points(cv2.resize(roi, (224, 224)),
+                                         self.avg_pts)
+            cv2.imshow("ModelSpace", model_space_image)
+
+            roi_space_image = display_tools.overlay_points(roi, roi_pts)
+            cv2.imshow("RoiSpace", roi_space_image)
+
             image = display_tools.draw_glasses(image, faces[0], self.avg_pts, name="camera")
 
-        image = display_tools.draw_face_boxes(image, faces)  # multiple faces can be detected
+        with_glasses_image = display_tools.draw_face_boxes(image, faces)  # multiple faces can be detected
+        cv2.imshow("With Shades", with_glasses_image)
 
 
 if __name__ == "__main__":
     process = ImageProcess()
     vid = cv2.VideoCapture(0) # define a video capture object
     model = model_manager.ModelManager()
+    face_cascade = cv2.CascadeClassifier('detector_architectures/haarcascade_frontalface_default.xml')
+
     while (True):
         ret, frame = vid.read() # Capture the video frame by frame
-        process.update_frame(frame, model)
+        frame = cv2.flip(frame, 1) # makes the cam mirror you - more intuitive
+        process.update_and_display_frame(frame, model, face_cascade)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break

modules/display_tools.py
@@ -2,21 +2,32 @@ import cv2
 import numpy as np
 from . import converters
 
-def overlay_points(image, pts, name):
+def overlay_points(image, pts):
     im_copy = np.copy(image)
     for point in pts:
         pt = (int(point[0].item()), int(point[1].item()))
         cv2.drawMarker(im_copy, pt, (0, 0, 255), markerType=cv2.MARKER_STAR,
                        markerSize=3, thickness=2, line_type=cv2.LINE_AA)
-    cv2.imshow(name, im_copy)
     return im_copy
 
 
-def extract_roi(image, roi_meta, pad=50):
+def extract_roi(image, roi_meta, pad=.25):
     (x, y, w, h) = roi_meta
+    pad = int( (w**2+h**2)**.5 *pad )
     im_height, im_width, channels = image.shape
-    roi = image[max(0, y - pad):min(im_height, y + h + pad),
-               max(0, x - pad):min(im_width, x + w + pad)]
+    top = y-pad
+    bottom = y+h+pad
+    left = x-pad
+    right = x + w + pad
+
+    # reliably error free, but performance starts to suffer as face squishes on edges
+    # roi = image[max(0, y - pad):min(im_height, y + h + pad),
+    #            max(0, x - pad):min(im_width, x + w + pad)]
+
+    if top < 0 or bottom > image.shape[0] or left < 0 or right > image.shape[1]:
+        return None
+
+    roi = image[top:bottom, left :right]
     return roi
 
 def draw_face_boxes(image, faces):
@@ -25,10 +36,8 @@ def draw_face_boxes(image, faces):
     for (x,y,w,h) in faces:
         cv2.rectangle(image_with_detections,(x,y),(x+w,y+h),(255,0,0),3)
     if len(faces) == 0:
-        cv2.imshow('camera', image)
         return image
     else:
-        cv2.imshow('camera', image_with_detections)
         return image_with_detections
 
     return
@@ -53,10 +62,10 @@ def draw_glasses(image, roi, pts_model, name="Glasses"):
 
 
     # Shift/scale glasses image to match face
-    w *=2
-    h *= 2
-    x -= w//4
-    y -= h//2
+    w = int(w*2.5)
+    h = int(h*2.5)
+    x = int(x-w//3.9)
+    y = int(y-h//2)
 
     # read and resize sunglasses
     sunglasses = cv2.imread('images/sunglasses.png', cv2.IMREAD_UNCHANGED) # read
@@ -72,6 +81,5 @@ def draw_glasses(image, roi, pts_model, name="Glasses"):
         # set the area of the image to the changed region with sunglasses
     image_copy[y:y + h, x:x + w] = roi_color
 
-    cv2.imshow(name, image_copy)
     return image_copy
 