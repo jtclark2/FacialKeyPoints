Bug/Chore: Improvements to scaling and naming (particularly image transforms) I had previously named a few image crops 'roi's...This wasn't wrong, but it was a bit ambiguous. There are really 3 spaces: - raw capture: The image that we start with - cropped: The cropped portion of the raw capture (no rescaling)     - Cropped is based on the haar cascade, with additional padding optionally     applied...thus I also distringuish between raw/padded now     - raw_face_data is just x,y,w,h for a cropped region (w/o padding) - model space: Rescaling of the cropped image to 224x224 image of a face,   which is ready to be passed into the model.

modules/converters.py
@@ -9,7 +9,7 @@ pad is a buffer optionally applied to the edge of roi
 
 import numpy as np
 
-def roi_from_model(pt_model, roi_shape, model_shape=(224, 224)):
+def crop_from_model(pt_model, roi_shape, model_shape=(224, 224)):
     """
     Convert coordinates from model back to the roi it was sampled from.
 
@@ -55,5 +55,5 @@ def camera_from_model(pt_model, roi_meta):
     roi_offset = np.array([roi_meta[0], roi_meta[1]])
     roi_shape = np.array([roi_meta[2], roi_meta[3]])
 
-    pt_camera = camera_from_roi(roi_from_model(pt_model, roi_shape), roi_offset)
+    pt_camera = camera_from_roi(crop_from_model(pt_model, roi_shape), roi_offset)
     return pt_camera

modules/display_tools.py
@@ -7,12 +7,12 @@ def overlay_points(image, pts):
     for point in pts:
         pt = (int(point[0].item()), int(point[1].item()))
         cv2.drawMarker(im_copy, pt, (0, 0, 255), markerType=cv2.MARKER_STAR,
-                       markerSize=3, thickness=2, line_type=cv2.LINE_AA)
+                       markerSize=1, thickness=2, line_type=cv2.LINE_AA)
     return im_copy
 
 
-def extract_roi(image, roi_meta, pad=.25):
-    (x, y, w, h) = roi_meta
+def extract_roi(image, face_data, pad):
+    (x, y, w, h) = face_data
     pad = int( (w**2+h**2)**.5 *pad )
     im_height, im_width, channels = image.shape
     top = y-pad
@@ -28,25 +28,26 @@ def extract_roi(image, roi_meta, pad=.25):
         return None
 
     roi = image[top:bottom, left :right]
-    return roi
+    padded_face_data = [x-pad, y-pad, w+2*pad, h+2*pad]
+    return padded_face_data, roi
 
-def draw_face_boxes(image, faces):
+def draw_face_boxes(image, faces_data):
     # loop over the detected faces, mark the image where each face is found
     image_with_detections = image.copy()
-    for (x,y,w,h) in faces:
+    for (x,y,w,h) in faces_data:
         cv2.rectangle(image_with_detections,(x,y),(x+w,y+h),(255,0,0),3)
-    if len(faces) == 0:
+    if len(faces_data) == 0:
         return image
     else:
         return image_with_detections
 
     return
 
-def draw_glasses(image, roi, pts_model, name="Glasses"):
+def draw_glasses(image, roi_data, pts_model, scale=1):
     """
     Display sunglasses on top of the image in the appropriate place
     """
-    key_pts = np.array([converters.camera_from_model(pt, roi) for pt in pts_model])
+    key_pts = np.array([converters.camera_from_model(pt, roi_data) for pt in pts_model])
 
     # copy of the face image for overlay
     image_copy = np.copy(image)
@@ -57,16 +58,24 @@ def draw_glasses(image, roi, pts_model, name="Glasses"):
     y = int(key_pts[17, 1])
 
     # height and width of sunglasses
-    h = int(abs(key_pts[27, 1] - key_pts[34, 1])) # h = length of nose
-    w = int(abs(key_pts[17, 0] - key_pts[26, 0])) # w = left to right eyebrow edges
+    h = int(abs(key_pts[27, 1] - key_pts[34, 1]) * scale) # h = length of nose
+    w = int(abs(key_pts[17, 0] - key_pts[26, 0]) * scale) # w = left to right eyebrow edges
 
 
     # Shift/scale glasses image to match face
-    w = int(w*2.5)
-    h = int(h*2.5)
-    x = int(x-w//3.9)
-    y = int(y-h//2)
+    # arbitrary constant to shift glasses a bit, since image corners aren't perfectly aligned with eye corners (TODO: moves magic number)
+    x = int(x + w//30)
+    y = int(y - h//10)
+    w = int(w)
+    h = int(h)
 
+    # Shift to adjust for scale factor
+    x -= int((scale-1)*w/2)
+    y -= int((scale-1)*h/2)
+
+    if(w == 0 or h == 0):
+        print("Warning: Image/glasses too small. Exiting without applying overlay.")
+    print("scale: ", scale, w, h)
     # read and resize sunglasses
     sunglasses = cv2.imread('images/sunglasses.png', cv2.IMREAD_UNCHANGED) # read
     new_sunglasses = cv2.resize(sunglasses, (w, h), interpolation=cv2.INTER_CUBIC) #resize

modules/model_manager.py
@@ -3,14 +3,17 @@ import numpy as np
 import torch
 from models import Net
 
-
 class ModelManager:
     def __init__(self):
+        """
+        This class initializes the model, loading a pre-trained model and setting eval mode (to disable dropout, etc.)
+        """
         """load saved model parameters"""
         model_dir = 'saved_models/'
         model_name = 'final_model.pt'  #
         self.model = Net()
         self.model.load_state_dict(torch.load(model_dir + model_name))
+        self.model.eval()
 
     def find_keypoints(self, roi_orig):
         """